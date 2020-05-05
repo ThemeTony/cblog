4766a0b1701b41ddf6ca4ecf088a22dd0ae36ce5
MatrixCoder-CHEN <a20070110a@qq.com>
2020-05-05 09:09:16 +0800

    Add installation and use instructions.

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# Placeholder
+# 特征与依赖
+- Markdown 写作支持（Parsedown 库）（暂不能关闭）
+- MathJax 数学公式渲染支持（可开启或关闭）
+- PJAX 无刷新体验 （不影响SEO）
+- RemixIcon 图标
+- Prism.js 代码高亮 
+- Algolia 搜索（您需要到官网注册获得id与秘钥）
+- Laravel 开发框架
+- Nprogress 加载进度条 （更好的用户体验）
+- Laravel-admin 强大的后台管理系统
+- Artalk 评论系统 
+- Bootstarap 制作的简洁但优雅的界面
+- 
+#要求
+- PHP >= 7.2.5
+- BCMath PHP 拓展
+- Ctype PHP 拓展
+- JSON PHP 拓展
+- Mbstring PHP 拓展
+- OpenSSL PHP 拓展
+- PDO PHP 拓展
+- Tokenizer PHP 拓展
+- XML PHP 拓展
+- Mysql Or Postgresql (推荐)
+
+#安装
+```
+//从github上克隆源码到本地
+git clone https://github.com/ThemeTony/cblog.git
+
+cd cblog
+php -r "copy('.env.example', '.env');"
+php artisan key:generate
+```
+然后，你需要设置`.env`，按照注释填写
+```
+php artisan migrate //生成数据表
+php artisan admin:install //如有报错，请直接忽视
+```
+
+请把运行目录设置在`./public`<br>
+给您的网站用户读写`./storage`的权限，这个目录是用来储存session、图片等文件的<br>
+做完以上步骤，您不出意外就可以跑起来了<br>
+您还需要设置评论板，依赖于[Artalk](https://github.com/qwqcode/Artalk)
+```
+cd ArtalkServerPhp
+php -r "copy('Config.example.php', 'Config.php');"
+```
+然后，你需要设置`Config.php`，按照注释填写，
+```
+// 确保当前在 ArtalkServerPhp 目录
+composer install 
+```
+，新建网站`ArtalkPhpServer/public`，给您的网站用户读写`ArtalkPhpServer/data`的权限，这个用来存放评论数据
+
+具体可见[Artalk-API-PHP](https://github.com/qwqcode/Artalk-API-PHP)
+
+之后，请更改`config/cblog.php`，按注释填写
+# 使用
+后台地址 www.example.com/admin
+- 账户 admin
+- 密码 admin
+
+在admin/menu选项卡中自定义菜单
+1. 文章管理的路径：/articles
+2. 页面管理的路径：/pages
+3. 分类管理的路径：/cates
+4. 标签管理的路径：/tags
+5. 顶栏管理的路径：/navs
+6. 导航分类管理的路径：/nav_cates
+
+
+
--- config/cblog.php
+++ config/cblog.php
@@ -1,25 +1,25 @@
 <?php
 return [
-    'website_name'=>"MatrixCoder's Blog",
-    'description'=>'An Poor Programmer and Student',
-    'avatar'=>'https://file.moetu.org/images/2020/03/20/k5NQhBId6H1D9tT71a9bbd12b762d30.jpg',
-    'github'=>'https://github.com/MatrixCoder-Chen',
-    'your_name'=>'MatrixCoder',
-    'math_tex'=>true,
-    'prism'=>true,
-    'registration'=>'123',
-    'algolia_id'=>env('ALGOLIA_APP_ID'),
-    'algolia_only_search_key'=>env('ALGOLIA_ONLY_SEARCH_KEY'),
+    'website_name'=>"MatrixCoder's Blog", # 网站名
+    'description'=>'An Poor Programmer and Student',# 网站描述
+    'avatar'=>'https://file.moetu.org/images/2020/03/20/k5NQhBId6H1D9tT71a9bbd12b762d30.jpg',# 头像
+    'github'=>'https://github.com/MatrixCoder-Chen',# github地址，没有请填写 null
+    'your_name'=>'MatrixCoder',# 你的名字
+    'math_tex'=>true,# 是否渲染数学公式
+    'prism'=>true,# 是否使用prism美化代码
+    'registration'=>null,# 备案号，没有请填写 null
+    'algolia_id'=>env('ALGOLIA_APP_ID'),# 不需要管
+    'algolia_only_search_key'=>env('ALGOLIA_ONLY_SEARCH_KEY'),# 不需要管
     'comment'=>[
-        'placeholder'=>"来啊，快活啊 ( ゜- ゜)",
-        'noComment'=>'快来成为第一个评论的人吧~',
+        'placeholder'=>"来啊，快活啊 ( ゜- ゜)",# 评论框的占位符
+        'noComment'=>'快来成为第一个评论的人吧~',# 没有评论时的显示
         'defaultAvatar'=> 'mp',
-        'pageKey'=> 'window.location.pathname',
-        'serverUrl'=> env('APP_URL').':8080',
-        'sendBtn'=>'Send',
+        'pageKey'=> 'window.location.pathname',# 页面唯一标识，如果您不知道这是什么，可以不管
+        'serverUrl'=> env('APP_URL').':8080',# Artalk的地址
+        'sendBtn'=>'Send',# 发送按钮的字符
         'readMore'=> [
-            'pageSize'=> 10,
-            'autoLoad'=> true
+            'pageSize'=> 10,# 每页显示评论个数
+            'autoLoad'=> true# 是否自动加载
         ]
     ]
 ];
