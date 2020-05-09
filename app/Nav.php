69a624f8075a4cd941f46c3d91ce0b14dc5d1e02
MatrixCoder-CHEN <a20070110a@qq.com>
2020-05-09 17:05:18 +0800

    Remove laravel-admin.

--- app/Nav.php
+++ app/Nav.php
@@ -3,19 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Encore\Admin\Traits\AdminBuilder;
-use Encore\Admin\Traits\ModelTree;
 class Nav extends Model
 {
-    use ModelTree, AdminBuilder;
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
-
-        $this->setParentColumn('parent_id');
-        $this->setOrderColumn('sort');
-        $this->setTitleColumn('name');
-    }
     public function parent()
     {
         return $this->belongsTo(Nav::class);
