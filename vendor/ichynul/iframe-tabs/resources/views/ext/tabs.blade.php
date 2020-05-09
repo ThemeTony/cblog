0272b87682ae666071a923571f2d5dbb107256b5
MatrixCoder-CHEN <a20070110a@qq.com>
2020-05-09 12:42:25 +0800

    Remove vendor.

--- vendor/ichynul/iframe-tabs/resources/views/ext/tabs.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="content-tabs">
-    <nav class="page-tabs menuTabs tab-ui-menu" id="tab-menu">
-        <div class="page-tabs-content" style="margin-left: 0px;">
-        </div>
-    </nav>
-</div>
